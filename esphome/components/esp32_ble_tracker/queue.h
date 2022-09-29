152fdaf231abb15013f47ba043806ee21010fd59
J. Nick Koston <nick@koston.org>
2022-09-28 14:25:01 -1000

    heap

--- esphome/components/esp32_ble_tracker/queue.h
+++ esphome/components/esp32_ble_tracker/queue.h
@@ -1,7 +1,9 @@
 #pragma once
 
 #ifdef USE_ESP32
-#include "esphome/core/component.h"
+#include "esphome/core/application.h"
+#include "esphome/core/defines.h"
+#include "esphome/core/hal.h"
 #include "esphome/core/helpers.h"
 #include "esphome/core/log.h"
 
