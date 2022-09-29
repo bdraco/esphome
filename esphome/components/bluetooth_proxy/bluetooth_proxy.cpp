c4d49cc93a1c150524f5369194b25708afba5a59
J. Nick Koston <nick@koston.org>
2022-09-28 17:26:11 -1000

    log

--- esphome/components/bluetooth_proxy/bluetooth_proxy.cpp
+++ esphome/components/bluetooth_proxy/bluetooth_proxy.cpp
@@ -21,8 +21,8 @@ BluetoothProxy::BluetoothProxy() {
 }
 
 bool BluetoothProxy::parse_device(const esp32_ble_tracker::ESPBTDevice &device) {
-  ESP_LOGV(TAG, "Proxying packet from %s - %s. RSSI: %d dB", device.get_name().c_str(), device.address_str().c_str(),
-           device.get_rssi());
+  //ESP_LOGV(TAG, "Proxying packet from %s - %s. RSSI: %d dB", device.get_name().c_str(), device.address_str().c_str(),
+  //         device.get_rssi());
   this->send_api_packet_(device);
 
   this->address_type_map_[device.address_uint64()] = device.get_address_type();
