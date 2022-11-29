ab7105c3104c78764a3b2c5588d42f35070f77de
J. Nick Koston <nick@koston.org>
2022-11-29 09:18:32 -1000

    format

--- esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
+++ esphome/components/esp32_ble_tracker/esp32_ble_tracker.cpp
@@ -574,18 +574,32 @@ std::string ESPBTUUID::to_string() const {
 }
 
 uint64_t ESPBTUUID::get_128bit_high() const {
-  esp_bt_uuid_t uuid = this->as_128bit().get_uuid();
-  return ((uint64_t) uuid.uuid.uuid128[15] << 56) | ((uint64_t) uuid.uuid.uuid128[14] << 48) |
-         ((uint64_t) uuid.uuid.uuid128[13] << 40) | ((uint64_t) uuid.uuid.uuid128[12] << 32) |
-         ((uint64_t) uuid.uuid.uuid128[11] << 24) | ((uint64_t) uuid.uuid.uuid128[10] << 16) |
-         ((uint64_t) uuid.uuid.uuid128[9] << 8) | ((uint64_t) uuid.uuid.uuid128[8]);
+  switch (this->uuid_.len) {
+    case ESP_UUID_LEN_16:
+      return (uint64_t) 0;
+    case ESP_UUID_LEN_32:
+      return (uint64_t) 0;
+    default:
+    case ESP_UUID_LEN_128:
+      return ((uint64_t) this->uuid_.uuid.uuid128[15] << 56) | ((uint64_t) this->uuid_.uuid.uuid128[14] << 48) |
+             ((uint64_t) this->uuid_.uuid.uuid128[13] << 40) | ((uint64_t) this->uuid_.uuid.uuid128[12] << 32) |
+             ((uint64_t) this->uuid_.uuid.uuid128[11] << 24) | ((uint64_t) this->uuid_.uuid.uuid128[10] << 16) |
+             ((uint64_t) this->uuid_.uuid.uuid128[9] << 8) | ((uint64_t) this->uuid_.uuid.uuid128[8]);
+  }
 }
 uint64_t ESPBTUUID::get_128bit_low() const {
-  esp_bt_uuid_t uuid = this->as_128bit().get_uuid();
-  return ((uint64_t) uuid.uuid.uuid128[7] << 56) | ((uint64_t) uuid.uuid.uuid128[6] << 48) |
-         ((uint64_t) uuid.uuid.uuid128[5] << 40) | ((uint64_t) uuid.uuid.uuid128[4] << 32) |
-         ((uint64_t) uuid.uuid.uuid128[3] << 24) | ((uint64_t) uuid.uuid.uuid128[2] << 16) |
-         ((uint64_t) uuid.uuid.uuid128[1] << 8) | ((uint64_t) uuid.uuid.uuid128[0]);
+  switch (this->uuid_.len) {
+    case ESP_UUID_LEN_16:
+      return (uint64_t) this->uuid_.uuid.uuid16;
+    case ESP_UUID_LEN_32:
+      return (uint64_t) this->uuid_.uuid.uuid32;
+    default:
+    case ESP_UUID_LEN_128:
+      return ((uint64_t) this->uuid_.uuid.uuid128[7] << 56) | ((uint64_t) this->uuid_.uuid.uuid128[6] << 48) |
+             ((uint64_t) this->uuid_.uuid.uuid128[5] << 40) | ((uint64_t) this->uuid_.uuid.uuid128[4] << 32) |
+             ((uint64_t) this->uuid_.uuid.uuid128[3] << 24) | ((uint64_t) this->uuid_.uuid.uuid128[2] << 16) |
+             ((uint64_t) this->uuid_.uuid.uuid128[1] << 8) | ((uint64_t) this->uuid_.uuid.uuid128[0]);
+  }
 }
 
 ESPBLEiBeacon::ESPBLEiBeacon(const uint8_t *data) { memcpy(&this->beacon_data_, data, sizeof(beacon_data_)); }
